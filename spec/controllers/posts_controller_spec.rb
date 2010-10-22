8ac58622238ca871d0ed56bf0f90f74763cd50c4
pctao <pc_tao@htc.com>
2010-10-23 01:46:55 +0800

    add posts controller pending test

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,18 @@
+require File.expand_path("../../spec_helper.rb", __FILE__)
+
+describe PostsController do
+  pending "GET index"
+
+  pending "GET show"
+
+  pending "GET new"
+
+  pending "POST create"
+
+  pending "GET edit"
+
+  pending "PUT update"
+
+  pending "DELETE destroy"
+end
+
