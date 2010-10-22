f3b2ac7d3ac495c60a05a006bd3002f2c148aec9
pctao <pc_tao@htc.com>
2010-10-23 00:48:06 +0800

    add post#new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,10 @@ class PostsController < ApplicationController
   before_filter :find_forum
   before_filter :find_post,  :except => [:index, :new, :create]
 
+  def new
+    @post = @forum.posts.build
+  end
+
   protected
   def find_forum
     @forum = Forum.find(params[:forum_id])
