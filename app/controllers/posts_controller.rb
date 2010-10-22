388320a2f4212c3cecd6323264543b5098e2a21d
pctao <pc_tao@htc.com>
2010-10-23 00:49:05 +0800

    add post#show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,9 @@ class PostsController < ApplicationController
   before_filter :find_forum
   before_filter :find_post,  :except => [:index, :new, :create]
 
+  def show
+  end
+
   def new
     @post = @forum.posts.build
   end
