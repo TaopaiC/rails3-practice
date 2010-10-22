6af65785e63e7f4f34715b61b798a0343cd479c0
pctao <pc_tao@htc.com>
2010-10-23 00:46:33 +0800

    add post before_filter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,13 @@
 class PostsController < ApplicationController
+  before_filter :find_forum
+  before_filter :find_post,  :except => [:index, :new, :create]
+
+  protected
+  def find_forum
+    @forum = Forum.find(params[:forum_id])
+  end
+
+  def find_post
+    @post = @forum.posts.find(params[:id])
+  end
 end
