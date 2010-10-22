1c1fa1cc18a3c5151c012f7cc72ec1ab338ed9d6
pctao <pc_tao@htc.com>
2010-10-23 01:21:34 +0800

    add forum controller index test

--- spec/controllers/forums_controller_spec.rb
+++ spec/controllers/forums_controller_spec.rb
@@ -1,7 +1,17 @@
 require File.expand_path("../../spec_helper.rb", __FILE__)
 
 describe ForumsController do
-  pending "GET index"
+  describe "GET index" do
+    it "returns all forums" do
+      @forums = [ mock_model(Forum) ]
+      Forum.stub(:all) { @forum }
+
+      get :index
+
+      assigns(:forums).should eq( @forum )
+      response.should render_template("index")
+    end
+  end
 
   pending "GET show"
 
