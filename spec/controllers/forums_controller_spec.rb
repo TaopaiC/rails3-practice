fba5b8f0e45d0cf559f1d21a2b27957f504da246
pctao <pc_tao@htc.com>
2010-10-23 01:22:46 +0800

    add forum controller edit test

--- spec/controllers/forums_controller_spec.rb
+++ spec/controllers/forums_controller_spec.rb
@@ -36,9 +36,42 @@ describe ForumsController do
     end
   end
 
-  pending "POST create"
+  describe "POST create" do
+    it "creates successfully" do
+      @forum = mock_model(Forum)
+      @params = { "title" => Faker::Lorem.sentence }
+      Forum.should_receive(:new).with(@params).and_return(@forum)
+      @forum.should_receive(:save).and_return(true)
 
-  pending "GET edit"
+      post :create, {:forum => @params}
+
+      response.should redirect_to(forum_posts_path(@forum))
+    end
+
+    it "fails to create" do
+      @forum = mock_model(Forum)
+      @params = { "title" => Faker::Lorem.sentence }
+      Forum.should_receive(:new).with(@params).and_return(@forum)
+      @forum.should_receive(:save).and_return(false)
+
+      post :create, {:forum => @params}
+
+      assigns(:forum).should eq(@forum)
+      response.should render_template("new")
+    end
+  end
+
+  describe "GET edit" do
+    it "returns requested forum" do
+      @forum = mock_model(Forum)
+      Forum.should_receive(:find).with(3).and_return(@forum)
+
+      get :edit, :id => 3
+
+      assigns(:forum).should eq(@forum)
+      response.should render_template("edit")
+    end
+  end
 
   pending "PUT update"
 
