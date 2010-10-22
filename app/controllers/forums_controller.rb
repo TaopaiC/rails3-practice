3be26c6a2eb6dfed13645975e296ca8f9b23bad9
pctao <pc_tao@htc.com>
2010-10-23 00:38:53 +0800

    add forum#new

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -1,6 +1,10 @@
 class ForumsController < ApplicationController
   before_filter :find_forum, :except => [:index, :new, :create]
 
+  def new
+    @forum = Forum.new
+  end
+
   protected
   def find_forum
     @forum = Forum.find(params[:id])
