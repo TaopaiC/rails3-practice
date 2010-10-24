67dcdd844139a5187da2d724d1c65fe4d93dbd0b
pctao <pc_tao@htc.com>
2010-10-24 17:28:14 +0800

    user content_tag in helpers

--- app/helpers/forums_helper.rb
+++ app/helpers/forums_helper.rb
@@ -1,36 +1,29 @@
 module ForumsHelper
   def new_forum_menu
-    menu = <<-end_src
-<div id="nav">
-  <ul>
-    <li>#{ link_to "Forum list", forums_path }</li>
-  </ul>
-</div>
-    end_src
-    menu.html_safe
+    content_tag(:div,
+      content_tag(:ul,
+        content_tag(:li, link_to("Forum list", forums_path))
+      ),
+    :id => "nav")
   end
 
   def forum_menu
-    menu = <<-end_src
-<div id="nav">
-  <ul>
-    <li>#{ link_to "Forum list", forums_path }</li>
-    <li>#{ link_to "Edit forum", edit_forum_path(@forum) }</li>
-    <li>#{ link_to "Destroy forum", forum_path(@forum), :method => :delete }</li>
-  </ul>
-</div>
-    end_src
-    menu.html_safe
+    content_tag(:div,
+      content_tag(:ul,
+        [
+        content_tag(:li, link_to("Forum list", forums_path)),
+        content_tag(:li, link_to("Edit forum", edit_forum_path(@forum))),
+        content_tag(:li, link_to("Destroy forum", forum_path(@forum), :method => :delete))
+        ].join
+      ),
+    :id => "nav")
   end
 
   def forums_menu
-    menu = <<-end_src
-<div id="nav">
-  <ul>
-    <li>#{ link_to "New Forum", new_forum_path }</li>
-  </ul>
-</div>
-    end_src
-    menu.html_safe
+    content_tag(:div,
+      content_tag(:ul,
+        content_tag(:li, link_to("New forum", new_forum_path))
+      ),
+    :id => "nav")
   end
 end
