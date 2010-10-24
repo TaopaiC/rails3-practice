907b5414751999b5f8fee84fa3545b99566ba723
pctao <pc_tao@htc.com>
2010-10-24 14:00:06 +0800

    add posts controller index test

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,7 +1,21 @@
 require File.expand_path("../../spec_helper.rb", __FILE__)
 
 describe PostsController do
-  pending "GET index"
+
+  describe "GET index" do
+    it "returns all posts" do
+      @forum = mock_model(Forum)
+      @posts = [mock_model(Post)]
+      controller.should_receive(:find_forum) { controller.instance_variable_set("@forum", @forum) }
+      @forum.should_receive(:posts).and_return(@posts)
+
+      get :index, :forum_id => 3
+
+      assigns(:forum).should eq( @forum )
+      assigns(:posts).should eq( @posts )
+      response.should render_template("index")
+    end
+  end
 
   pending "GET show"
 
